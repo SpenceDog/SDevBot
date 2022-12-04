ac4e8d3892a89aa02e33bc2161b8e66cffc2477a
Spencer Lynch <accounts@spencerlynch.us>
2022-12-04 02:57:11 -0500

    Quality of life improvements.

--- Bot_Commands/dndapi.py
+++ Bot_Commands/dndapi.py
@@ -6,12 +6,16 @@ def dnd_api(usr_command):
     line_break = "```" # Used to simulate a linebreak on Discord.
     command_message = " " # Generic string that we return the value of any of our searches.
 
-    dnd_search = str(usr_command.content).split(begin_command)[1]  # Gets the string after the '$5e' deliminator in the command.
-    if dnd_search.lower() == "classes": # If we searched for classes...
-        command_message = dnd_api_classes_general(dnd_search, line_break)
+    try:
+        dnd_search = str(usr_command.content).split(begin_command)[1]  # Gets the string after the '$5e' deliminator in the command.
+        if dnd_search.lower() == "classes":  # If we searched for classes...
+            command_message = dnd_api_classes_general(dnd_search, line_break)
 
-    else:
+        else:
+            command_message = line_break + 'Use this command to search the D&D 5e SRD Content. Can specifiy content by entering "Classes" as a parameter.' + line_break
+    except:
         command_message = line_break + 'Use this command to search the D&D 5e SRD Content. Can specifiy content by entering "Classes" as a parameter.' + line_break
+
 
     return str(command_message) # Sends it in the channel it was called from.
 
