63522af5da75f36169452a87ba5b24ea6deb09e9
Spencer Lynch <accounts@spencerlynch.us>
2022-12-06 00:04:31 -0500

    Classes are searchable
    Can now search classes, returns just the name of the class.

--- Bot_Commands/DND_API/dnd_api_classes_general.py
+++ Bot_Commands/DND_API/dnd_api_classes_general.py
@@ -1,10 +1,10 @@
 import requests
 from Bot_Commands.DND_API.API_For_List import dnd_api_for_list
 
-def dnd_api_classes_general(command_search, break_line):
+def dnd_api_classes_general(command_search, break_line, api_data_query):
     http_response = requests.get("https://www.dnd5eapi.co/api/" + command_search + "/")  # Call the API.
     api_data = http_response.json()  # Saves the raw JSON of our request.
-    resultsList = api_data['results']  # Gets the total amount of results.
+    resultsList = api_data[api_data_query]  # Gets the total amount of results.
     api_response = dnd_api_for_list(http_response, resultsList, break_line) # Converts the list into readable output.
 
 
--- Bot_Commands/dndapi.py
+++ Bot_Commands/dndapi.py
@@ -1,3 +1,4 @@
+import requests
 from Bot_Commands.DND_API.dnd_api_classes_general import dnd_api_classes_general
 
 def dnd_api(usr_command):
@@ -6,12 +7,21 @@ def dnd_api(usr_command):
     command_message = " " # Generic string that we return the value of any of our searches.
 
     try:
-        dnd_search = str(usr_command.content).split(begin_command)[1]  # Gets the string after the '$5e' deliminator in the command.
+        dnd_search = str(usr_command.content.lower()).split(begin_command)[1]  # Gets the string after the '$5e' deliminator in the command.
         if dnd_search.lower() == "classes":  # If we searched for classes...
-            command_message = dnd_api_classes_general(dnd_search, line_break)
+            command_message = dnd_api_classes_general(dnd_search, line_break, 'results')
 
         else:
-            command_message = line_break + 'Use this command to search the D&D 5e SRD Content. Can specifiy content by entering "Classes" as a parameter.' + line_break
+            class_search = str(dnd_search).split("classes ")[1] # Splits the message after classes.
+            search_query = "classes/" + class_search # Builds our query.
+            print(search_query)
+
+            http_response = requests.get("https://www.dnd5eapi.co/api/" + search_query + "/")  # Call the API.
+            api_data = http_response.json()  # Saves the raw JSON of our request.
+
+            command_message = api_data["hit_die"] # Returns our query.
+
+
     except:
         command_message = line_break + 'Use this command to search the D&D 5e SRD Content. Can specifiy content by entering "Classes" as a parameter.' + line_break
 
